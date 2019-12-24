e65e93ac171673f3d177e937e2c62d614630afb6
Seph <paulo.seph@hotmail.com>
2019-12-23 21:32:59 -0300

    Atualizando arquivos

--- Firstproject/area_circulo_v7.py
+++ Firstproject/area_circulo_v7.py
@@ -1,9 +1,12 @@
 #!python
 from math import pi
 
-# if se modulo é uma condição true or false, se for igual == main ele vai executar as ações a baixo que estão indentadas a ela, com espaços.
-# ou sejá, a condição funciona para ./area_circulo_v7.py no terminal, ai vai puxar essa condição, de outra froma não.
-# (import area_circulo_v7 não vai executar mais.)
+#   if se modulo é uma condição true or false, se for igual == main ele vai
+#   executar as ações a baixo que estão indentadas a ela, com espaços.
+#   ou sejá, a condição funciona para ./area_circulo_v7.py no terminal, ai vai
+#  puxar essa condição, de outra froma não.
+#  (import area_circulo_v7 não vai executar mais.)
+
 
 if __name__ == '__main__':
     raio = input('Informe o raio: ')
